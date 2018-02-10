a75ff9cd9d803906225ac2daa4422de727ca5d73
Rondellio <ron.walters23@gmail.com>
2018-02-10 08:48:35 -0500

    Ant Header - Ron

--- Ant.h
+++ Ant.h
@@ -1,1 +1,13 @@
+#ifndef ANT_HPP
+#define ANT_HPP
+#include "Critter.hpp"
 
+class Ant: public Critter
+{
+  public:
+    Ant();
+    virtual void move(char**, int, int);
+    virtual void breed(char**, int, int);
+};
+
+#endif
